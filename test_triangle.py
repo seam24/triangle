09e76fdb1352dfd274fd9917f8a5c62189e83664
jjsohn92 <kasiossh555@gmail.com>
2025-05-06 16:36:56 +0900

    Add tests for equilateral and tests for additional invalids check

--- test_triangle.py
+++ test_triangle.py
@@ -5,3 +5,12 @@ def test_invalid1():
 
 def test_isosceles():
     assert triangle(3, 2, 3) == 1
+
+def test_equilateral():
+    assert triangle(3, 3, 3) == 2  
+    
+def test_invalid2():
+    assert triangle(1, 2, 3) == -1  # a + b <= c
+
+def test_invalid3():
+    assert triangle(10, 1, 1) == -1  # a + b < c
